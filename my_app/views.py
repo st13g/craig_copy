0ebb28a4e3c645e27ec114de4994f178935627df
st13g <cristdelc@gmail.com>
2020-04-11 17:18:27 -0500

    add requirements

--- my_app/views.py
+++ my_app/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
-
+import request
+from bs4 import BeautifulSoup
 # Create your views here.
 def home(request):
     return render(request,'base.html')
+
+def new_search(request):
+    search = request.POST.get('search')
+    stuff = {'search':search,
+    }
+    return render(request, 'my_app/new_search.html',stuff)
