596ba4aee4807e30e7767fae88723522981204e8
st13g <cristdelc@gmail.com>
2020-04-11 23:27:37 -0500

    last part

--- craig_cp/settings.py
+++ craig_cp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^y0&&#z1l)k3+uf8ci48ss7o3c58*j&bri6s9ri7!!_8#i3=xn'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','127.0.0.1','192.168.0.107']
 
 
 # Application definition
--- my_app/views.py
+++ my_app/views.py
@@ -1,12 +1,38 @@
 from django.shortcuts import render
-import request
+import requests
 from bs4 import BeautifulSoup
+from requests.compat import quote_plus
+from . import models
 # Create your views here.
+BASE_CRAIGLIST_URL = 'https://lima.craigslist.org/search/hhh?query={}'
+
 def home(request):
     return render(request,'base.html')
 
 def new_search(request):
     search = request.POST.get('search')
-    stuff = {'search':search,
+    models.Search.objects.create(search=search)
+    final_url= BASE_CRAIGLIST_URL.format(quote_plus(search))
+    response = requests.get(final_url)
+    data = response.text
+    soup = BeautifulSoup(data, features='html.parser')
+
+    post_listings = soup.find_all('li',{'class':'result-row'})
+
+
+    final_postings = []
+    for post in post_listings:
+        post_title = post.find(class_='result-title').text
+        post_url = post.find('a').get('href')
+        if post.find(class_='result-price'):
+            post_price = post.find(class_='result-price').text
+        else:
+            post_price = 'N/A'
+        
+        final_postings.append((post_title, post_url, post_price))
+
+    stuff = {
+    'search':search,
+    'final_postings': final_postings,
     }
     return render(request, 'my_app/new_search.html',stuff)
